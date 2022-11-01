e969c1e74e9242a66225ae595aed4d08f1e04076
Mohamad <80650043+MDshirzad@users.noreply.github.com>
2022-11-01 23:44:58 +0330

    Add files via upload

---
+++ ipchecker.py
@@ -0,0 +1,31 @@
+from requests.auth import HTTPBasicAuth
+import requests
+import json
+import time
+import subprocess
+
+
+key="YOU_KEY"
+ 
+url = f'https://api.ipgeolocation.io/ipgeo?apiKey={key}'
+
+headers = {'Accept': 'application/json'}
+ 
+
+while 1:
+
+	req = requests.get(url, headers=headers)
+	js =req.json()
+
+	ip=0
+	country=''
+
+	for key, value in js.items():
+		if (key == 'ip'):
+	 		ip=value 		
+		elif (key == 'country_name'):
+	 		country=value
+	 		
+	subprocess.run(["/usr/bin/notify-send", "--icon=error", country]) #making notification in linuxe
+	time.sleep(10) #per second 		
+
